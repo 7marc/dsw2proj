05fc17f53df4f3c2d4e02b01d7c4a32cfe60dac7
7marc <marceloyudi.ok@gmail.com>
2024-08-17 18:00:38 -0300

    update index final

--- DSW2_FRONT/pages/index.vue
+++ DSW2_FRONT/pages/index.vue
@@ -1,49 +1,3 @@
-<!--
-
-<template>
-  <div>
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Description</th>
-          <th scope="col">Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(item, index) in items" :key="index">
-          <td>{{ item.name }}</td>
-          <td>{{ item.description }}</td>
-          <td>{{ item.price | currency }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</template>
-
-<script>
-export default 
-{
-  data() {
-    return {
-      items: [
-        { name: 'Item 1', description: 'Description 1', price: 10 },
-        { name: 'Item 2', description: 'Description 2', price: 20 },
-        { name: 'Item 3', description: 'Description 3', price: 30 }
-      ]
-    }
-  },
-  filters: {
-    currency(value) 
-    {
-      return '$${parseFloat(value).toFixed(2)}';
-    }
-  }
-}
-</script>
-
--->
-
 
 <template>
 
@@ -63,12 +17,44 @@ export default
   
     <div>
       <h2>Home</h2>
-      <p>mucho texto</p>
+      <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Description</th>
+          <th scope="col">Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in items" :key="index">
+          <td>{{ item.name }}</td>
+          <td>{{ item.description }}</td>
+          <td>{{ item.price | currency }}</td>
+        </tr>
+      </tbody>
+    </table>
     </div>
   </template>
   
-  <script setup>
-  
+  <script>
+  export default 
+  {
+    data() {
+      return {
+        items: [
+          { name: 'Item 1', description: 'Description 1', price: 10 },
+          { name: 'Item 2', description: 'Description 2', price: 20 },
+          { name: 'Item 3', description: 'Description 3', price: 30 }
+        ]
+      }
+    },
+    filters: {
+      currency(value) 
+      {
+        return '$${parseFloat(value).toFixed(2)}';
+      }
+    }
+  }
   </script>
   
   <style scoped>
